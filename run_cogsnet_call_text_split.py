ae560d454843e693645c75be7b28a013f67ca8ef
Ross Devito <devitr2@ganxis3.nest.rpi.edu>
2019-10-17 10:18:47 -0400

    text call split for cogsnet now works and has changes made to regular cogsnet model to improve performance

--- run_cogsnet_call_text_split.py
+++ run_cogsnet_call_text_split.py
@@ -9,6 +9,8 @@ import pandas as pd
 import dask
 from dask.distributed import Client, LocalCluster
 
+import rbo  # https://github.com/changyaochen/rbo
+
 
 class ParamGrid(object):
 	"""
@@ -25,7 +27,8 @@ class ParamGrid(object):
 
 	def get_grid(self):
 		return product(self.L, self.mu_call, self.mu_text,
-						self.theta, forget_types)
+                 self.theta, forget_types)
+
 
 def jaccard_similarity(list1, list2):
 	s1 = set(list1)
@@ -47,11 +50,8 @@ def forget_func(forget_type, time_delta, forget_intensity):
 		return np.e ** (-1 * forget_intensity * time_delta)
 
 
-def get_signals(start_times, observation_times, event_type, 
-				mu_0, forget_intensity_0, mu_1, forget_intensity_1, 
-				theta, forget_type):
-	start_times = sorted(start_times)  # remove sorts for speedup
-	observation_times = sorted(observation_times)
+def get_signals_2_class(start_times, event_types, observation_times,
+                          mu_0, mu_1, theta, forget_type, forget_intensity):
 
 	ret_values = []
 
@@ -66,7 +66,7 @@ def get_signals(start_times, observation_times, event_type,
 			if obs_ind >= total_obs:
 				return ret_values
 
-		if event_type[0] == 0:
+		if event_types[0] == 0:
 			current_signal = mu_0
 		else:
 			current_signal = mu_1
@@ -101,6 +101,11 @@ def get_signals(start_times, observation_times, event_type,
                                                 forget_intensity)
 		if decayed_signal < theta:
 			decayed_signal = 0
+
+		if event_types[0] == 0:
+			mu = mu_0
+		else:
+			mu = mu_1
 		current_signal = mu + decayed_signal * (1 - mu)
 
 	while obs_ind < total_obs:
@@ -119,37 +124,62 @@ def get_signals(start_times, observation_times, event_type,
 	return ret_values
 
 
-# @dask.delayed
+@dask.delayed
 def evaluate_for_node(events, surveys, param_grid):
 	return_matrix = []
 
-	for L, mu_t, mu_c, theta, forget_type in param_grid.get_grid():
-		forget_intensity_c = get_forget_intensity(L, mu_c, theta, forget_type)
-		forget_intensity_t = get_forget_intensity(L, mu_t, theta, forget_type)
+	survey_times = sorted(list(surveys.keys()))
+	node_ids = np.asarray(list(events.keys()))
 
-		node_ids = np.asarray(list(events.keys()))
-		survey_times = list(surveys.keys())
+	event_times = []
+	event_types = []
+
+	for events_mat in events.values():
+		event_order = np.argsort(events_mat[:, 2])
+		event_times.append(events_mat[event_order][:, 2])
+		event_types.append(events_mat[event_order][:, 0])
+
+	for L, mu_t, mu_c, theta, forget_type in param_grid.get_grid():
+		if theta >= mu_t or theta >= mu_c:
+			continue
+		forget_intensity = get_forget_intensity(L, (mu_c + mu_t) / 2, theta, 
+												forget_type)
 
 		signal_strengths = np.asarray(
-			[get_signals(events_mat[:, 2], survey_times, events_mat[:, 1], 
-							mu_c, forget_intensity_c,
-                            mu_t, forget_intensity_t,
-							theta, forget_type)
-				for events_mat in events.values()]
+			[get_signals_2_class(times, types, survey_times,
+					mu_t, mu_c, theta, forget_type, forget_intensity)
+				for times, types in zip(event_times, event_types)]
 		)
 
 		for i in range(len(survey_times)):
 			top_n = list(surveys[survey_times[i]].values())
+			cogsnet_top_n = node_ids[(-signal_strengths[:, i]).argsort()[:len(top_n)]]
 
-			return_matrix.append([L, mu, theta, forget_type, jaccard_similarity(
-				top_n,
-				node_ids[(-signal_strengths[:, i]).argsort()[:len(top_n)]]
-			)])
+			return_matrix.append(
+                            [L, mu_t, mu_c, theta, forget_type,
+                             jaccard_similarity(top_n, cogsnet_top_n),
+                                rbo.RankingSimilarity(
+                                 top_n, cogsnet_top_n).rbo()
+                             ])
 
 	return return_matrix
 
 
 def evaluate_model_params(edge_dict, interaction_dict, survey_dict, param_grid):
+	"""
+	Given interaction data, survey data, and lists of parameters to check,
+		creates a dataframe with a row for each combination of parameters.
+		The combination of parameters will have the average jaccard similarity
+		and rank-biased overlap (RBO) across all surveys.
+
+	Creates a list of dask delayed processes, each of which handle one node who
+		has survey data.  
+
+	Return example:
+		     L   mu_t   mu_c  theta forget_func  jaccard_sim       rbo
+		0  1.0    0.1    0.2   0.05         exp     0.240194  0.298805
+		1  2.0    0.1    0.2   0.05         exp     0.286562  0.327788
+	"""
 	res_matrix = []
 
 	n = 1
@@ -165,52 +195,65 @@ def evaluate_model_params(edge_dict, interaction_dict, survey_dict, param_grid):
 
 	res_matrix = np.vstack(dask.compute(res_matrix)[0])
 
-	return res_matrix
+	res_df = pd.DataFrame(
+				res_matrix, 
+				columns=['L', 'mu_t', 'mu_c', 'theta', 'forget_func', 
+							'jaccard_sim', 'rbo'])
+
+	res_df[['L', 'mu_t', 'mu_c', 'theta', 'jaccard_sim', 'rbo']
+        ] = res_df[['L', 'mu_t', 'mu_c', 'theta', 'jaccard_sim', 'rbo']].astype(float)
+	res_df.L = res_df.L / 24
+
+	return res_df
 
 
 if __name__ == "__main__":
-	# cluster = LocalCluster(n_workers=24, dashboard_address=':8776')
-	# client = Client(cluster)
+	# Create dask cluster
+	cluster = LocalCluster(n_workers=70, dashboard_address=':8766')
+	client = Client(cluster)
 
+	# Load required dicts
 	with open(os.path.join("data", "edge_dict.pkl"), 'rb') as pkl:
 		edge_dict = pickle.load(pkl)
 
 	with open(os.path.join("data", "interaction_dict.pkl"), 'rb') as pkl:
 		interaction_dict = pickle.load(pkl)
 
-	with open(os.path.join("data", "survey_dict.pkl"), 'rb') as pkl:
+	with open(os.path.join("data", "survey_textcall_dict.pkl"), 'rb') as pkl:
 		survey_dict = pickle.load(pkl)
 
 	# create values which will be used in grid search
 
-	# run 1
-	# L_vals = np.asarray(range(1, 365, 2)) * 24
-	L_vals = np.asarray([365, 400, 460])
-	mu_vals = np.asarray([.4, .6, .8])
-	theta_vals = np.asarray([.05, .1, .3, .5])
-	forget_types = ['pow', 'exp']
+	# # # run 3 validation
+	# L_vals = np.asarray([460]) * 24
+	# mu_vals = np.asarray([.2])
+	# theta_vals =np.asarray([.166667])
+	# forget_types = ['exp']
 
-	param_grid = ParamGrid(L_vals, mu_vals, mu_vals, 
-							theta_vals, forget_types)
+	# run 6 val
+	L_vals = np.asarray([1, 4, 7, 14, 21, 28, 60, 90, 120, 180, 365, 460, 730]) * 24
+	mu_vals = np.linspace(.002, .9, 20)
+	theta_vals = np.linspace(.001, .5, 20)
+	forget_types = ['exp']
 
+	param_grid = ParamGrid(L_vals, mu_vals, mu_vals,
+                        theta_vals, forget_types)
+
+	# Preform grid search to create dataframe of parameters combination and
+	# their respective performances
 	start_time = time.time()
 
-	results = evaluate_model_params(edge_dict, interaction_dict, survey_dict,
-                                 	param_grid)
+	res_df = evaluate_model_params(edge_dict, interaction_dict, survey_dict,
+                                	param_grid)
 
 	print(time.time() - start_time)
 
-	res_df = pd.DataFrame(
-		results, columns=['L', 'mu', 'theta', 'forget_func', 'jaccard_sim'])
-	res_df[['L', 'mu', 'theta', 'jaccard_sim']
-        ] = res_df[['L', 'mu', 'theta', 'jaccard_sim']].astype(float)
-	res_df.L = res_df.L / 24
-
+	# Format and save results
 	mean_df = res_df.groupby(
-		['L', 'mu', 'theta', 'forget_func']).mean().reset_index()
-	mean_df.to_csv(os.path.join('results', 'mean_df_cts.csv'))
+		['L', 'mu_t', 'mu_c', 'theta', 'forget_func']).mean().reset_index()
+	mean_df.to_csv(os.path.join('results', 'mean_df_split.csv'))
 	med_df = res_df.groupby(['L', 'mu', 'theta', 'forget_func']).median().reset_index()
-	med_df.to_csv(os.path.join('results', 'med_df_cts.csv'))
+	med_df.to_csv(os.path.join('results', 'med_df_split.csv'))
 
-	# client.close()
-	# cluster.close()
+	client.close()
+	cluster.close()
